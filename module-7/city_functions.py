5ae7274ebcd5eadf8bee29f5faab4fdc55839d23
MikeGordon <migordon@my365.bellevue.edu>
2026-09-17 22:16:46 -0700

    added python program, test program, and word doc

---
+++ module-7/city_functions.py
@@ -0,0 +1,24 @@
+# Write a function that accepts two parameters: a city name and a country name.
+# The function should return a single string of the form City, Country, such as Santiago, Chile.
+
+def city_country(city, country, population='', language=''):
+    if population and language:
+        location_data = (
+            f'\n{city}, {country} - population {population}, {language}\n')
+    elif population:
+        location_data = (
+            f'\n{city}, {country} - population {population}\n')
+    else:
+        location_data = (f'\n{city}, {country}\n')
+    return location_data
+
+
+def main(): {
+    print(city_country('Peoria', 'USA')),
+    print(city_country('Santiago', 'Chile', 5000)),
+    print(city_country('Farmington', 'USA', 47000, 'English'))
+}
+
+
+if __name__ == '__main__':
+    main()
---
+++ module-7/test_cities.py
@@ -0,0 +1,24 @@
+import unittest
+from city_functions import city_country
+
+
+class LocationTestCase(unittest.TestCase):
+    """Tests for 'city_country.py"""
+
+    def test_city_country(self):
+        formatted_location = city_country('Peoria', 'USA')
+        self.assertEqual(formatted_location, '\nPeoria, USA\n')
+
+    def test_city_country_population(self):
+        formatted_location = city_country('Santiago', 'Chile', 5000)
+        self.assertEqual(formatted_location,
+                         '\nSantiago, Chile - population 5000\n')
+
+    def test_city_country_population_language(self):
+        formatted_location = city_country('Santiago', 'Chile', 5000, 'Spanish')
+        self.assertEqual(formatted_location,
+                         '\nSantiago, Chile - population 5000, Spanish\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
