97d6b7f7ba79f0e12ce7091bd7a7bdcc92e283c6
MikeGordon <migordon@my365.bellevue.edu>
2026-09-19 13:01:37 -0700

    wrote python code to meet assignment requirements

---
+++ module-8/student_json.py
@@ -0,0 +1,51 @@
+# Michael Gordon
+# CSD325 Advanced Python
+# Module 8.2
+
+import json
+
+# Open the file and use the JSON load() function to load the file into a Python class list
+with open('student.json', 'r') as file:
+    students = json.load(file)
+
+
+# Create a function that loops through the .json class list and prints out each value
+def student_values(file):
+    for student in file:
+        first_name = student['F_Name']
+        last_name = student['L_Name']
+        student_id = student['Student_ID']
+        email = student['Email']
+
+        print(f'{last_name}, {first_name} : ID = {student_id}, Email = {email}')
+
+
+# Output notification to the user that this is the original Student list
+print('\nThis is the original Student list.')
+
+
+# Call your print function
+student_values(students)
+
+
+# Add your last name, first name, fictional ID, and email to the class list using append()
+students.append({
+    "F_Name": "Mike",
+    "L_Name": "Gordon",
+    "Student_ID": 99999,
+    "Email": "migordon@my365.bellevue.edu"
+})
+
+# Output notification to the user that this is the updated Student list
+print('\nThis is the updated Student list.')
+
+
+# Call your print function
+student_values(students)
+
+# Use the JSON dump() function to append the new data to the .json file
+with open('Student.json', 'w') as json_file:
+    json.dump(students, json_file)
+
+# Output notification to the user that the .json file was updated
+print('Successfully appended to the JSON file!')
